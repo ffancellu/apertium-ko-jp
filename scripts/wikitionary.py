d37420b2cc9e0685215517229ff9371b5caa8a31
Federico Fancellu <ffancellu@cngl.ie>
2014-02-17 23:01:39 +0000

    Minor changes to wikitionary.py

--- scripts/wikitionary.py
+++ scripts/wikitionary.py
@@ -14,6 +14,18 @@ class Wikictionary:
     def __init__(self):
 
         self.url = "http://en.wiktionary.org/w/api.php"
+        self.JSONObject = None
+
+        
+        # {'english_word':
+        #     {'korean':
+        #         {'translation': 'a',
+        #          'transl': 'b'},
+        #     {'japanese':
+        #         {'translation': 'a',
+        #          'other': 'b'}
+        #          }
+        #         }
 
     def lookup_word(self,word):
 
@@ -48,26 +60,32 @@ class Wikictionary:
         dnw = revision.index('\n\n')
         revision = revision[:dnw]
 
-        trans_lines = list()
-
         for lang in args:
 
-            print lang
-        
-            trans_lines.append(re.findall(r'\* ' + lang + r': \{\{(.+)\}\}',revision)[0])
+            trans_lines = dict()
 
+            trans = re.findall('\* ' + lang + r': (.+)',revision)[0]
+
+            #TODO: just takes into consideration the 1st lexical translation
+            trans = trans.split('}}, {{')[0]
+
+            trans_lines[lang] = {
+                'translation': trans.split('|')[2],
+                'other' : trans.split('|')[3]}
+
+        print trans_lines
         return trans_lines
         
-    def extract_trans(self,trans_lines):
+    # def extract_trans(self,trans_lines):
 
-        translations = map(lambda x: x.split('|')[2],trans_lines)
-        translit = map(lambda x: x.split('|')[3],trans_lines)
+    #     translations = map(lambda x: x.split('|')[2],trans_lines)
+    #     translit = map(lambda x: x.split('|')[3],trans_lines)
 
-        return translations, translit
+    #     return translations, translit
 
 if __name__=="__main__":
 
     wk = Wikictionary()
-    revision = wk.lookup_word('rain')
+    revision = wk.lookup_word('abbreviate')
     trans_lines = wk.isolate_trans(revision,'Korean','Japanese')
     wk.extract_trans(trans_lines)
